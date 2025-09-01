917fa2f2ec36c9d9190ad29a17689c1ea9e80a7a
Robin Singh <vougishtiger@gmail.com>
2025-09-01 03:32:24 -0700

    Random Object Spawning

--- dreamscape_3d.py
+++ dreamscape_3d.py
@@ -6,53 +6,88 @@ app= Ursina()
 
 player= FirstPersonController()
 ground= Entity(model= 'plane', collider= 'box', scale= 100, texture= 'white_cube', texture_scale= (100,100))
+
 stats= {"Courage": 0, "Fear": 0, "Creativity": 0, "Doubt": 0}
 
-door= Entity(model='cube', color=color.azure, position=(3,0,3), scale=(1.5,3,0.2))
-mask= Entity(model= 'sphere', color=color.red, position=(-2,1,4))
-statue= Entity(model='cube', color=color.gray, position=(0,0,6), scale=(1,2,1))
+choice_text= Text("", origin=(0,0), y=-0.35, scale= 1.2)
+stats_text= Text(f"Stats: {stats}", origin= (-.5, .5), background= True)
 
-choice_text= Text("",  origin=(0,0), y=- 0.35, scale= 1.2)
-stats_text = Text(f"Stats: {stats}", origin=(-.5,.5), background=True)
-
-dream_choices= {
+dream_objects= {
   "door": {
-    "face it": {"Courage": 2, "Fear": -1},
-    "avoid it": {"Doubt": 2, "Fear": 1},
-    "imagine a new path": {"Creativity": 2},
-    "scream": {"Fear": 2}
+    "model": "cube",
+    "color": color.azure, 
+    "choices": {
+      "face it": {"Courage": 2, "Fear": -1},
+      "avoid it": {"Doubt": 2, "Fear": 1},
+      "imagine a new path": {"Creativity": 2},
+      "scream": {"Fear": 2}
+    }
   },
   "mask": {
-    "wear it": {"Creativity": 2},
-    "smash it": {"Courage": 2, "Fear": 1},
-    "ignore it": {"Doubt": 2}
+    "model": "sphere",
+    "color": color.red,
+    "choices": {
+      "wear it": {"Creativity": 2},
+      "smash it": {"Courage": 2, "Fear": 1},
+      "ignore it": {"Doubt": 2}
+    }
   },
   "statue": {
-    "pray": {"Doubt": -1, "Courage": 1},
-    "deface": {"Fear": 2, "Creativity": 1},
-    "walk away": {"Doubt": 2}
-  }
+    "model": "cube",
+    "color": color.gray,
+    "choices": {
+      "pray": {"Doubt": -1, "Courage": 1},
+      "deface": {"Fear": 2, "Creativity": 1},
+      "walk away": {"Doubt": 2}
+      }
+    },
+  "clock": {
+    "model": "cube",
+    "color": color.yellow,
+    "choices": {
+      "wind it": {"Creativity": 1},
+      "smash it": {"Courage": 1, "Fear": 1},
+      "ignore it": {"Doubt": 2}
+      }
+    },
+  "book": {
+    "model": "cube",
+    "color": color.green,
+    "choices": {
+      "read it": {"Creativity": 2},
+      "burn it": {"Courage": 1, "Fear": 1},
+      "close it": {"Doubt": 2}
+      }
+    }
 }
 
+spawned_objects= []
 current_interaction= None
 
-def apply_choice(object_name, choice):
+def spawn_dream_objects(num= 5):
+  for name, data in random.sample(list(dream_objects.items()), num):
+    x, z = random.randint(-10, 10), random.randint(-10, 10)
+    entity= Entity(
+      model= data["model"],
+      color= data["color"],
+      position= (x, 0, z),
+      collider= "box", 
+      scale= 1.5
+    )
+    spawned_objects.append({"name": name, "entity": entity})
+
+spawn_dream_objects(num= 5)
+
+def apply_choice(obj_name, choice):
   global stats, current_interaction
-  effects= dream_choices[object_name][choice]
+  effects= dream_objects[obj_name]["choices"][choice]
   for stat, change in effects.items():
-    stats[stat]+= change
-  stats_text.text= f"Stats: {stats}"
-  choice_text.text= f"You chose '{choice}'. The dream shifts..."
-
-  if object_name == "door":
-    door.color= color.lime
-  elif object_name == "mask":
-    mask.color= color.orange
-  elif object_name == "statue":
-    statue.color= color.violet  
-
-  current_interaction= None
+    stats[stat] += change
+  stats_text.text = f"Stats: {stats}"
+  choice_text.text = f"You chose '{choice}'. The dream shifts..."
+  current_interaction = None
   check_wake_up()
+
 
 
 def check_wake_up():
@@ -68,39 +103,26 @@ def check_wake_up():
 
 def end_game(message):
   choice_text.text= ""
+  stats_text.text = ""
   Text(message, origin= (0,0), scale= 2, color=color.yellow)
   player.disable()
 
 def update():
   global current_interaction
-
-  if distance(player, door)< 2:
-    current_interaction= "door"
-    choice_text.text= "The door whispers your name...\n[1] face it  [2] avoid it  [3] imagine a new path  [4] scream"
-  elif distance(player, mask)< 2:
-    current_interaction= "mask"
-    choice_text.text= "The mask shifts colors...\n[1] wear it [2] smash it [3] ignore it"
-  elif distance(player, statue)< 2:
-    current_interaction= "statue"
-    choice_text.text= "The statue hums softly...\n[1] pray  [2] deface  [3] walk away"
-  else: 
-    current_interaction= None
+  current_interaction = None
+  for obj in spawned_objects:
+    if distance(player, obj["entity"]) < 2:
+      current_interaction = obj["name"]
+      options= list(dream_objects[obj["name"]]["choices"].keys())
+      numbered= [f"[{i+1}] {opt}" for i,opt in enumerate(options)]
+      choice_text.text= f"The {obj['name']} shimmers...\n" + "  ".join(numbered)
+      break
 
 def input(key):
   if current_interaction:
-    if current_interaction == "door":
-      if key == '1': apply_choice("door", "face it")
-      elif key == '2': apply_choice("door", "avoid it")
-      elif key == '3': apply_choice("door", "imagine a new path")
-      elif key == '4': apply_choice("door", "scream")
-    elif current_interaction == "mask":
-      if key == '1': apply_choice("mask", "wear it")
-      elif key == '2': apply_choice("mask", "smash it")
-      elif key == '3' : apply_choice("mask", "ignore it")
-    elif current_interaction == "statue":
-      if key == '1': apply_choice("statue", "pray")
-      elif key == '2': apply_choice("statue", "deface")
-      elif key == '3': apply_choice("statue", "walk away")
+    options= list(dream_objects[current_interaction]["choices"].keys())
+    if key in ["1", "2", "3", "4"] and int(key)-1 < len(options):
+      apply_choice(current_interaction,options[int(key)-1])
 
 Sky()
 DirectionalLight().look_at(Vec3(1,-1,-1))
